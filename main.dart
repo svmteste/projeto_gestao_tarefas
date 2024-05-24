9ba11ed5ba368f5ca43508eb02f226de0ed6ef50
Suzana Mota <45774928+svmteste@users.noreply.github.com>
2024-05-24 17:28:38 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,1 +1,24 @@
 
+import 'package:flutter/material.dart';
+import 'initial_screen.dart';
+import 'add_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.purple),
+        useMaterial3: true,
+      ),
+      home: AddScreen(),
+    );
+  }
+}
