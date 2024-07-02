988b4ef4d4a0375a7eb2f9f58a9d940ee4b044b3
Suzana Mota <45774928+svmteste@users.noreply.github.com>
2024-07-02 16:33:32 -0300

    Create task_list_screen.dart

---
+++ task_list_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'task_add_screen.dart';
+import 'task_edit_screen.dart';
+
+class TaskListScreen extends StatelessWidget {
+  final CollectionReference tasks = FirebaseFirestore.instance.collection('tasks');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Task List'),
+      ),
+      body: StreamBuilder(
+        stream: tasks.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          return ListView(
+            children: snapshot.data!.docs.map((task) {
+              return ListTile(
+                title: Text(task['name']),
+                subtitle: Text(task['image']),
+                trailing: IconButton(
+                  icon: Icon(Icons.delete),
+                  onPressed: () {
+                    tasks.doc(task.id).delete();
+                  },
+                ),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => TaskEditScreen(task: task),
+                    ),
+                  );
+                },
+              );
+            }).toList(),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TaskAddScreen()),
+          );
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
