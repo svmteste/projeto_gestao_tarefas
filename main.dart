9db302b69da6066b7746f734e5179b6e6ee12c53
Suzana Mota <45774928+svmteste@users.noreply.github.com>
2024-07-02 17:37:34 -0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter CRUD Demo',
+      title: 'Flutter CRUD - Gestão Tarefas IFNMG',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
