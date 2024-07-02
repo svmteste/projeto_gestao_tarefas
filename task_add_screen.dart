4f11ffe9fa4ccd54040a20b2be2c6cdba1403523
Suzana Mota <45774928+svmteste@users.noreply.github.com>
2024-07-02 17:37:01 -0300

    Update task_add_screen.dart

--- task_add_screen.dart
+++ task_add_screen.dart
@@ -9,30 +9,39 @@ class TaskAddScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Add Task'),
+        title: Text('Adicionar Tarefas'),
       ),
+
+
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
             TextField(
               controller: nameController,
-              decoration: InputDecoration(labelText: 'Name'),
+              decoration: InputDecoration(labelText: 'Tarefa'),
             ),
             TextField(
               controller: imageController,
-              decoration: InputDecoration(labelText: 'Image URL'),
+              decoration: InputDecoration(labelText: 'URL'),
             ),
             SizedBox(height: 20),
+
             ElevatedButton(
               onPressed: () {
-                FirebaseFirestore.instance.collection('tasks').add({
+                // Gerando um ID personalizado para o documento
+                String customID = 'Tarefa_'+ DateTime.now().millisecondsSinceEpoch.toString();
+                
+                FirebaseFirestore.instance.collection('tasks').doc(customID).set({
                   'name': nameController.text,
                   'image': imageController.text,
+                }).then((_) {
+                  Navigator.pop(context);
+                }).catchError((error) {
+                  print('Erro ao adicionar tarefa: $error');
                 });
-                Navigator.pop(context);
               },
-              child: Text('Add Task'),
+              child: Text('Adicionar Tarefa'),
             ),
           ],
         ),
@@ -40,3 +49,19 @@ class TaskAddScreen extends StatelessWidget {
     );
   }
 }
+//             ElevatedButton(
+//               onPressed: () {
+//                 FirebaseFirestore.instance.collection('tasks').add({
+//                   'name': nameController.text,
+//                   'image': imageController.text,
+//                 });
+//                 Navigator.pop(context);
+//               },
+//               child: Text('Adicionar Tarefa'),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
