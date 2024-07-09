3991328c9bf55d535f60f1786b2effef56c4f98d
Suzana Mota <45774928+svmteste@users.noreply.github.com>
2024-07-08 22:03:40 -0300

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,52 @@
+// Based on https://dartpad.dev/?id=d57c6c898dabb8c6fb41018588b8cf73
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'task_list_screen.dart';
+
+const Color darkBlue = Color.fromARGB(0, 47, 18, 43);
+
+const messageLimit = 30;
+
+
+void main() async {
+  const FirebaseOptions(
+  apiKey: "AIzaSyBhycJZ7wHsFYGDcdoZRazutKVNfEKa2SQ",
+  authDomain: "ifnmg-esporte.firebaseapp.com",
+  projectId: "ifnmg-esporte",
+  storageBucket: "ifnmg-esporte.appspot.com",
+  messagingSenderId: "844194342463",
+  appId: "1:844194342463:web:67655c8443b7a3c7e23a8a",
+  measurementId: "G-CTVQNN48YC");
+ WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp(
+      options: const FirebaseOptions(
+  apiKey: "AIzaSyBhycJZ7wHsFYGDcdoZRazutKVNfEKa2SQ",
+  authDomain: "ifnmg-esporte.firebaseapp.com",
+  projectId: "ifnmg-esporte",
+  storageBucket: "ifnmg-esporte.appspot.com",
+  messagingSenderId: "844194342463",
+  appId: "1:844194342463:web:67655c8443b7a3c7e23a8a",
+  measurementId: "G-CTVQNN48YC"));
+ runApp(MyApp());
+ FirebaseFirestore firestore = FirebaseFirestore.instance;
+ firestore.collection('Teste').doc('Teste_aula_mobile').set({'nome': 'Suzana Zapp'});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter CRUD - Gestão Tarefas IFNMG',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TaskListScreen(),
+    );
+  }
+}
+
